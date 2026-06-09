9618c06293fd3ec07a873ee5e32883c521e75d8a
tarangkumar44 <tarangkumar44@gmail.com>
2026-06-09 08:04:35 +0530

    Initial react basics

---
+++ Temp/app.js
@@ -0,0 +1,5 @@
+export var a = 20;
+// export default a;
+
+const name = "Sagar";
+export default name;
---
+++ Temp/script.js
@@ -0,0 +1,16 @@
+import name from './app.js'
+import {a} from './app.js'
+let h2 = document.createElement("h2");
+h2.innerHTML = "This is heading 2";
+console.log(h2);
+
+console.log(a);
+console.log(name);
+
+
+
+
+
+
+
+
